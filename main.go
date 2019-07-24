8f4bed5bd1f3bbdb903addaa9d31a2b789db2351
Renze Yu <renzeyu@microsoft.com>
2019-07-24 13:06:14 +0800

    deploy storage

--- main.go
+++ main.go
@@ -26,8 +26,16 @@ func commands() {
 			Action: deploy,
 			Flags: []cli.Flag{
 				cli.StringFlag{
+					Name:  "name",
+					Usage: "site name, determine the name of resource created (required)",
+				},
+				cli.StringFlag{
 					Name:  "subscription-id",
-					Usage: "Azure subscription id",
+					Usage: "Azure subscription id (required)",
+				},
+				cli.StringFlag{
+					Name:  "resource-group",
+					Usage: "Azure resource group (required)",
 				},
 			},
 		},
@@ -45,12 +53,46 @@ func main() {
 }
 
 func deploy(c *cli.Context) {
-	fmt.Println("deploying ...")
-	fmt.Println("subscription id:", c.String("subscription-id"))
-	out, err := exec.Command("cmd", "/c", "az account list --output table").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("subscriptions: %s\n", out)
-	fmt.Println("Complete!")
+	name := getFlag(c, "name")
+	group := getFlag(c, "resource-group")
+	sub := getFlag(c, "subscription-id")
+
+	storage := fmt.Sprintf("%sdocfxsite", name)
+	deployStorage(storage, group, sub)
 }
+
+func deployStorage(name string, group string, subscription string) {
+	fmt.Println("Deploying storage...")
+	execute(fmt.Sprintf("az storage account create -n %s -g %s --subscription %s --kind StorageV2", name, group, subscription))
+	execute(fmt.Sprintf("az storage blob service-properties update --account-name %s --static-website --index-document index.html", name))
+	fmt.Println("Finish deploying storage.")
+}
+
+func getFlag(c *cli.Context, flag string) string {
+	result := c.String(flag)
+	if result == "" {
+		log.Fatal(fmt.Sprintf("missing flag: %s", flag))
+	}
+	return result
+}
+
+func execute(str string) string {
+	fmt.Println("[DEBUG] ", str)
+	cmd := exec.Command("cmd", "/c", str)
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("%s", stdoutStderr))
+	}
+	return fmt.Sprintf("%s\n", stdoutStderr)
+}
+
+/*
+func execute(command string) string {
+	fmt.Println(command)
+	out, err := exec.Command("cmd", "/c", command).Output()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("%s: %s", err, out))
+	}
+	return fmt.Sprintf("%s", out)
+}
+*/
